ba739bd522175a2148edce71b7f71e558a22bd66
최용권 <chldydrnjs94@gmail.com>
2019-12-19 01:01:54 +0900

    html test

--- router/map/map.js
+++ router/map/map.js
@@ -1,28 +1,13 @@
 /* ==================== START modules ==================== */
-const mysql         = require('mysql');
-const dbConfig      = require('../../config/dbConfig');
+
 const express       = require('express');
-const app           = express();
 const router        = express.Router();
 const bodyParser    = require('body-parser');
-const crypto        = require('crypto');
+const fs = require('fs');
+
 /* ==================== END modules ==================== */
 
-/* ==================== START DB Connection ==================== */
-var dbOptions = {
-    host: dbConfig.host,
-    port: dbConfig.port,
-    user: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.database
-};
-
-var connection = mysql.createConnection(dbOptions);
-connection.connect();
-/* ==================== END DB Connection ==================== */
-
-
-router.get("/map", (request, response) => {
+router.get("/", (request, response) => {
     response.writeHead(200, {
         'Content-Type' : text/html
     });
@@ -31,10 +16,7 @@ router.get("/map", (request, response) => {
             response.writeHead(404);
             response.write("404 not found!");
         }
-        else {
-            response(data);
-        }
-        response.end();
+        response.end(data);
     });
     // response.end(fs.readFileSync(__dirname + './views/map.html'));
     response.sendFile(path.join(__dirname, '../views/map.html'));
