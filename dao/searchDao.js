7380c4395e7971bbc48970c25d02fd2ceaedd15d
hiYong94 <chl9741@naver.com>
2020-01-21 02:06:47 +0900

    Fix sql query
    * search API sql query modify

--- dao/searchDao.js
+++ dao/searchDao.js
@@ -14,7 +14,8 @@ Search.searchByName = function(name, response, next) {
         db((error, connection) => {
             let sqlQuery = "SELECT p.placeNumber, p.name, p.address, p.latitude, p.longitude, group_concat(k.name separator '|') AS 'keywordName' " + 
                                 "FROM place p, place_has_keyword phk, keyword k " + 
-                                "WHERE p.placeNumber = phk.placeNumber AND phk.keywordNumber = k.keywordNumber AND p.name like ?"
+                                "WHERE p.placeNumber = phk.placeNumber AND phk.keywordNumber = k.keywordNumber AND p.name like ? " +
+                                "GROUP BY p.placeNumber"
             connection.query(sqlQuery, '%' + name + '%', function(error, results) {
                 if (error) {
                     console.log("error: ", error)
