3745325b010f8ff531b1efbaf0b7b232c4057998
hiYong94 <chl9741@naver.com>
2020-01-30 00:20:07 +0900

    Delete semicolon

--- controller/imagesController.js
+++ controller/imagesController.js
@@ -5,9 +5,9 @@ const Images = require('../service/imagesService.js');
 /* ==================== END modules ==================== */
 
 exports.images_getImageFile = function(request, response, next) {
-    Images.getImageFile(request, response, next);
+    Images.getImageFile(request, response, next)
 }
 
 exports.images_uploadImageFile = function(request, response, next) {
-    Images.uploadImageFile(request, response, next);
+    Images.uploadImageFile(request, response, next)
 }
