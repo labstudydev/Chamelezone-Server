f626e24d7a1339f155c9f3efa14d8ff56daa3170
hiYong94 <chl9741@naver.com>
2020-02-12 20:00:50 +0900

    Fix 'search' and 'map' select query

--- dao/mapDao.js
+++ dao/mapDao.js
@@ -12,10 +12,21 @@ var Map = function(map) {
 Map.selectPlaceByName = function(name, response) {
     try {
         db((error, connection) => {
-            let selectPlaceByNameSqlQuery = "SELECT p.placeNumber, p.name, p.address, p.latitude, p.longitude, group_concat(k.name separator ', ') AS 'keywordName' " + 
-                                            "FROM place p, place_has_keyword phk, keyword k " + 
-                                            "WHERE p.placeNumber = phk.placeNumber AND phk.keywordNumber = k.keywordNumber AND p.name like ? " +
-                                            "GROUP BY p.placeNumber"
+            let selectPlaceByNameSqlQuery = `SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, A.keywordNumber, A.keywordName, ` +
+                                            `GROUP_CONCAT(PI.imageNumber SEPARATOR ',') AS 'imageNumber', ` +
+                                            `GROUP_CONCAT(PI.originalImageName SEPARATOR ',') AS 'originalImageName', ` +
+                                            `GROUP_CONCAT(PI.savedImageName SEPARATOR ',') AS 'savedImageName' ` +
+                                            `FROM place P ` +
+                                            `LEFT JOIN place_images PI ON PI.placeNumber = P.placeNumber ` +
+                                            `LEFT JOIN (SELECT PHK.placeNumber, ` +
+                                            `            GROUP_CONCAT(K.keywordNumber SEPARATOR ',')  AS 'keywordNumber', ` +
+                                            `            GROUP_CONCAT(K.name SEPARATOR ',')  AS 'keywordName' ` +
+                                            `            FROM place_has_keyword PHK ` +
+                                            `            LEFT JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+                                            `            GROUP BY PHK.placeNumber ` +
+                                            `            ORDER BY keywordNumber DESC) A ON A.placeNumber = P.placeNumber ` +
+                                            `WHERE P.name LIKE ? ` +
+                                            `GROUP BY P.placeNumber`
             connection.query(selectPlaceByNameSqlQuery, '%' + name + '%', function(error, results) {
                 if (error) {
                     console.log("error: ", error)
--- dao/searchDao.js
+++ dao/searchDao.js
@@ -13,10 +13,21 @@ var Search = function(search) {
 Search.selectByPlaceName = function(name, response) {
     try {
         db((error, connection) => {
-            let selectByPlaceNameSqlQuery = "SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, GROUP_CONCAT(K.name SEPARATOR ',') AS 'keywordName' " + 
-                                            "FROM place P, place_has_keyword PHK, keyword K " + 
-                                            "WHERE P.placeNumber = PHK.placeNumber AND PHK.keywordNumber = K.keywordNumber AND P.name like ? " +
-                                            "GROUP BY P.placeNumber"
+            let selectByPlaceNameSqlQuery = `SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, A.keywordNumber, A.keywordName, ` +
+                                            `GROUP_CONCAT(PI.imageNumber SEPARATOR ',') AS 'imageNumber', ` +
+                                            `GROUP_CONCAT(PI.originalImageName SEPARATOR ',') AS 'originalImageName', ` +
+                                            `GROUP_CONCAT(PI.savedImageName SEPARATOR ',') AS 'savedImageName' ` +
+                                            `FROM place P ` +
+                                            `LEFT JOIN place_images PI ON PI.placeNumber = P.placeNumber ` +
+                                            `LEFT JOIN (SELECT PHK.placeNumber, ` +
+                                            `            GROUP_CONCAT(K.keywordNumber SEPARATOR ',')  AS 'keywordNumber', ` +
+                                            `            GROUP_CONCAT(K.name SEPARATOR ',')  AS 'keywordName' ` +
+                                            `            FROM place_has_keyword PHK ` +
+                                            `            LEFT JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+                                            `            GROUP BY PHK.placeNumber ` +
+                                            `            ORDER BY keywordNumber DESC) A ON A.placeNumber = P.placeNumber ` +
+                                            `WHERE P.name LIKE ? ` +
+                                            `GROUP BY P.placeNumber`
             connection.query(selectByPlaceNameSqlQuery, '%' + name + '%', function(error, results) {
                 if (error) {
                     console.log("error: ", error)
@@ -37,10 +48,21 @@ Search.selectByPlaceName = function(name, response) {
 Search.selectByAreaName = function(name, response) {
     try {
         db((error, connection) => {
-            let selectByAreaNameSqlQuery = "SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, GROUP_CONCAT(K.name SEPARATOR ',') AS 'keywordName' " + 
-                                            "FROM place P, place_has_keyword PHK, keyword K " + 
-                                            "WHERE P.placeNumber = PHK.placeNumber AND PHK.keywordNumber = K.keywordNumber AND P.address like ? " +
-                                            "GROUP BY P.placeNumber"
+            let selectByAreaNameSqlQuery = `SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, A.keywordNumber, A.keywordName, ` +
+                                            `GROUP_CONCAT(PI.imageNumber SEPARATOR ',') AS 'imageNumber', ` +
+                                            `GROUP_CONCAT(PI.originalImageName SEPARATOR ',') AS 'originalImageName', ` +
+                                            `GROUP_CONCAT(PI.savedImageName SEPARATOR ',') AS 'savedImageName' ` +
+                                            `FROM place P ` +
+                                            `LEFT JOIN place_images PI ON PI.placeNumber = P.placeNumber ` +
+                                            `LEFT JOIN (SELECT PHK.placeNumber, ` +
+                                            `            GROUP_CONCAT(K.keywordNumber SEPARATOR ',')  AS 'keywordNumber', ` +
+                                            `            GROUP_CONCAT(K.name SEPARATOR ',')  AS 'keywordName' ` +
+                                            `            FROM place_has_keyword PHK ` +
+                                            `            LEFT JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+                                            `            GROUP BY PHK.placeNumber ` +
+                                            `            ORDER BY keywordNumber DESC) A ON A.placeNumber = P.placeNumber ` +
+                                            `WHERE P.address LIKE ? ` +
+                                            `GROUP BY P.placeNumber`
             connection.query(selectByAreaNameSqlQuery, '%' + name + '%', function(error, results) {
                 if (error) {
                     console.log("error: ", error)
@@ -61,10 +83,21 @@ Search.selectByAreaName = function(name, response) {
 Search.selectByKeywordName = function(name, response) {
     try {
         db((error, connection) => {
-            let selectByKeywordNameSqlQuery = "SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, GROUP_CONCAT(K.name SEPARATOR ',') AS 'keywordName' " + 
-                                                "FROM place P, place_has_keyword PHK, keyword K " + 
-                                                "WHERE P.placeNumber = PHK.placeNumber AND PHK.keywordNumber = K.keywordNumber AND K.name like ? " +
-                                                "GROUP BY P.placeNumber"
+            let selectByKeywordNameSqlQuery = `SELECT P.placeNumber, P.name, P.address, P.latitude, P.longitude, A.keywordNumber, A.keywordName, ` +
+                                                `GROUP_CONCAT(PI.imageNumber SEPARATOR ',') AS 'imageNumber', ` +
+                                                `GROUP_CONCAT(PI.originalImageName SEPARATOR ',') AS 'originalImageName', ` +
+                                                `GROUP_CONCAT(PI.savedImageName SEPARATOR ',') AS 'savedImageName' ` +
+                                                `FROM place P ` +
+                                                `LEFT JOIN place_images PI ON PI.placeNumber = P.placeNumber ` +
+                                                `LEFT JOIN (SELECT PHK.placeNumber, ` +
+                                                `            GROUP_CONCAT(K.keywordNumber SEPARATOR ',')  AS 'keywordNumber', ` +
+                                                `            GROUP_CONCAT(K.name SEPARATOR ',')  AS 'keywordName' ` +
+                                                `            FROM place_has_keyword PHK ` +
+                                                `            LEFT JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+                                                `            GROUP BY PHK.placeNumber ` +
+                                                `            ORDER BY keywordNumber DESC) A ON A.placeNumber = P.placeNumber ` +
+                                                `WHERE A.keywordName LIKE ? ` +
+                                                `GROUP BY P.placeNumber`
             connection.query(selectByKeywordNameSqlQuery, '%' + name + '%', function(error, results) {
                 if (error) {
                     console.log("error: ", error)
