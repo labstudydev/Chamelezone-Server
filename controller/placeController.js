b7f730448471e46becab855fdd1fa45d2e4eaaea
hiYong94 <chl9741@naver.com>
2020-02-17 22:00:45 +0900

    Fix typo

--- controller/placeController.js
+++ controller/placeController.js
@@ -1,29 +1,29 @@
 /* ==================== START modules ==================== */
 
-const Place = require('../service/placeService.js');
+const Place = require('../service/placeService.js')
 
 /* ==================== END modules ==================== */
 
 exports.place_create = function(request, response, next) {
-    Place.createPlace(request, response, next);
-};
+    Place.createPlace(request, response, next)
+}
 
 exports.place_readOne = function(request, response, next) {
-    Place.readOnePlace(request, response, next);
-};
+    Place.readOnePlace(request, response, next)
+}
 
 exports.place_readAll = function(request, response, next) {
-    Place.readAllPlace(request, response, next);
-};
+    Place.readAllPlace(request, response, next)
+}
 
 exports.place_update = function(request, response, next) {
-    Place.updatePlace(request, response, next);
-};
+    Place.updatePlace(request, response, next)
+}
 
 exports.place_delete = function(request, response, next) {
-    Place.deletePlace(request, response, next);
-};
+    Place.deletePlace(request, response, next)
+}
 
 exports.place_getCutrrentLocation = function(request, response, next) {
-    Place.getCutrrentLocation(request, response, next);
+    Place.getCutrrentLocation(request, response, next)
 }
