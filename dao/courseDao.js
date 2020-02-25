cf704b8dd63345dfa6c3b9dffc37be7034b24fd4
hiYong94 <chl9741@naver.com>
2020-02-25 23:06:58 +0900

    Fix 'course' regiDate date format and Add selectOneCourseSqlQuery

--- dao/courseDao.js
+++ dao/courseDao.js
@@ -77,7 +77,7 @@ Course.insertCourse = function([memberNumber, title, content, setImageArray, set
 Course.selectAllCourse = function(response) {
     try {
         db((error, connection) => {
-            const selectAllCourseSqlQuery = `SELECT C.courseNumber, C.title, C.regiDate, M.memberNumber, M.nickName , CI.imageNumber, CI.savedImageName ` +
+            const selectAllCourseSqlQuery = `SELECT C.courseNumber, C.title, DATE_FORMAT(C.regiDate, '%Y-%m-%d') as regiDate, M.memberNumber, M.nickName , CI.imageNumber, CI.savedImageName ` +
                                             `FROM course C ` +
                                             `LEFT JOIN member M ON M.memberNumber = C.memberNumber ` +
                                             `LEFT JOIN course_images CI ON CI.courseNumber = C.courseNumber ` +
@@ -102,8 +102,61 @@ Course.selectAllCourse = function(response) {
 Course.selectOneCourse = function([courseNumber], response) {
     try {
         db((error, connection) => {
-            const selectOneCourseSqlQuery = 
-            connection.query(selectOneCourseSqlQuery, function(error, results) {
+            // const selectOneCourseSqlQuery = `select JSON_ARRAY(C.courseNumber, C.title, C.content, CI.savedImageName) AS course, B.placeNumber, B.placeName, B.placeAddress, B.keywordName ` +
+            // `from course C ` +
+            // `LEFT JOIN course_images CI ON CI.courseNumber = C.courseNumber ` +
+            // `left join (select CHP.courseNumber, ` +
+            // `            group_concat(CHP.placeNumber separator ',') as placeNumber, ` +
+            // `            group_concat(P.name separator ',') as placeName, ` +
+            // `            group_concat(P.address separator ',') as placeAddress, ` +
+            // `            group_concat(A.keywordName separator '|') as keywordName ` +
+            // `            from course_has_place CHP ` +
+            // `           left join place P on P.placeNumber = CHP.placeNumber ` +
+            // `            left join (select PHK.placeNumber, GROUP_CONCAT(K.keywordNumber SEPARATOR ',') AS 'keywordNumber', JSON_OBJECT("keywordName", JSON_ARRAY(GROUP_CONCAT(K.name SEPARATOR ','))) AS 'keywordName' ` +
+            // `                    FROM place_has_keyword PHK ` +
+            // `                    JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+            // `                    GROUP BY PHK.placeNumber) A on A.placeNumber = CHP.placeNumber ` +
+            // `            group by courseNumber) B on B.courseNumber = C.courseNumber ` +
+            // `where C.courseNumber = ? ` +
+            // `group by C.courseNumber`
+
+            //이게원래
+            // const selectOneCourseSqlQuery = `select C.courseNumber, C.title, C.content, CI.savedImageName, B.placeNumber, B.placeName, B.placeAddress, B.keywordName ` +
+            //                                 `from course C ` +
+            //                                 `LEFT JOIN course_images CI ON CI.courseNumber = C.courseNumber ` +
+            //                                 `left join (select CHP.courseNumber, ` +
+            //                                 `            group_concat(CHP.placeNumber separator ',') as placeNumber, ` +
+            //                                 `            group_concat(P.name separator ',') as placeName, ` +
+            //                                 `            group_concat(P.address separator ',') as placeAddress, ` +
+            //                                 `            group_concat(A.keywordName separator '|') as keywordName ` +
+            //                                 `            from course_has_place CHP ` +
+            //                                 `           left join place P on P.placeNumber = CHP.placeNumber ` +
+            //                                 `            left join (select PHK.placeNumber, GROUP_CONCAT(K.keywordNumber SEPARATOR ',') AS 'keywordNumber', GROUP_CONCAT(K.name SEPARATOR ',') AS 'keywordName' ` +
+            //                                 `                    FROM place_has_keyword PHK ` +
+            //                                 `                    JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+            //                                 `                    GROUP BY PHK.placeNumber) A on A.placeNumber = CHP.placeNumber ` +
+            //                                 `            group by courseNumber) B on B.courseNumber = C.courseNumber ` +
+            //                                 `where C.courseNumber = ? ` +
+            //                                 `group by C.courseNumber`
+
+            const selectOneCourseSqlQuery = `select C.courseNumber, C.title, C.content, CI.savedImageName, B.placeNumber, B.placeName, B.placeAddress, B.keywordName ` +
+                                            `from course C ` +
+                                            `LEFT JOIN course_images CI ON CI.courseNumber = C.courseNumber ` +
+                                            `left join (select CHP.courseNumber, ` +
+                                            `            group_concat(DISTINCT CHP.placeNumber separator ',') as placeNumber, ` +
+                                            `            group_concat(DISTINCT P.name separator ',') as placeName, ` +
+                                            `            group_concat(DISTINCT P.address separator ',') as placeAddress, ` +
+                                            `            group_concat(DISTINCT A.keywordName separator '|') as keywordName ` +
+                                            `            from course_has_place CHP ` +
+                                            `           left join place P on P.placeNumber = CHP.placeNumber ` +
+                                            `            left join (select DISTINCT PHK.placeNumber, GROUP_CONCAT(DISTINCT K.keywordNumber SEPARATOR ',') AS 'keywordNumber', GROUP_CONCAT(DISTINCT K.name SEPARATOR ',') AS 'keywordName' ` +
+                                            `                        FROM place_has_keyword PHK ` +
+                                            `                        JOIN keyword K ON K.keywordNumber = PHK.keywordNumber ` +
+                                            `                        GROUP BY PHK.placeNumber) A on A.placeNumber = CHP.placeNumber ` +
+                                            `                        group by CHP.placeNumber) B on B.courseNumber = C.courseNumber ` +                      
+                                            `where C.courseNumber = ?`
+
+            connection.query(selectOneCourseSqlQuery,[courseNumber], function(error, results) {
                 if (error) {
                     console.log("error: ", error)
                     connection.release()
