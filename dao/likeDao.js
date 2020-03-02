04a8b4c78c71735d61d43b638ae4ceab828a6ff8
hiYong94 <chl9741@naver.com>
2020-03-03 05:34:14 +0900

    Fix 'like' sql query'

--- dao/likeDao.js
+++ dao/likeDao.js
@@ -27,11 +27,11 @@ Like.insertLike = function([placeNumber, memberNumber], response) {
     }
 }
 
-Like.deleteLike = function([likeNumber, placeNumber, memberNumber], response) {
+Like.deleteLike = function([placeNumber, memberNumber], response) {
     try {
         db((error, connection) => {
-            const deleteLikeSqlQuery = `DELETE FROM like_history WHERE likeNumber = ? AND placeNumber = ? AND memberNumber = ?`
-            connection.query(deleteLikeSqlQuery, [likeNumber, placeNumber, memberNumber], function(error, results) {
+            const deleteLikeSqlQuery = `DELETE FROM like_history WHERE placeNumber = ? AND memberNumber = ?`
+            connection.query(deleteLikeSqlQuery, [placeNumber, memberNumber], function(error, results) {
                 if (error) {
                     console.log("error: ", error)
                     connection.release()
