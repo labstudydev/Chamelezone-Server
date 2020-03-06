212333979133d311b407b67b9cbdb73b5889e676
hiYong94 <chl9741@naver.com>
2020-03-06 16:16:37 +0900

    Delete 'app.js' cleanup code

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-/* ==================== START modules ==================== */
-
-const { ErrorHandler, handleError } = require('./costomModules/customError')
-const express       = require('express')
-const hbs           = require('express-handlebars')
-const app           = express()
-const bodyParser    = require('body-parser')
-const router        = require('./router/index')
-
-/* ==================== END modules ==================== */
+const { ErrorHandler, handleError }     = require('./costomModules/customError')
+const express                           = require('express')
+const hbs                               = require('express-handlebars')
+const app                               = express()
+const bodyParser                        = require('body-parser')
+const router                            = require('./router/index')
 
 app.engine('hbs', hbs({
     extname: 'hbs',
@@ -16,22 +12,22 @@ app.engine('hbs', hbs({
     partialsDir:__dirname+'/views/partials'    
 }))
 
-app.set("view engine", "hbs")
+app.set('view engine', 'hbs')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/image', express.static(__dirname + "/public/uploads"))
+app.use('/image', express.static(__dirname + '/public/uploads'))
 app.use(router)
 
 app.use((error, request, response, next) => {
     handleError(error, response)
-    console.log(__filename + " == handleError : " + error.statusCode + ", message: " + error.message)
+    console.log(__filename + ' == handleError: ' + error.statusCode + ', message: ' + error.message)
 })
 
-app.get("/addressSearch", (request, response) => {
+app.get('/addressSearch', (request, response) => {
     response.status(200).render('map/addressSearch.hbs')
 })
 
 app.listen(3000, () => {
-    console.log("The server is running on Port 3000")
+    console.log('The server is running on Port 3000')
 })
