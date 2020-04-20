dcd532f6fd78c45821b7ec5d7f0be2588c2c07a2
hiYong94 <chl9741@naver.com>
2020-04-21 03:37:48 +0900

    Fix https auto-sni lib

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { ErrorHandler, handleError , NullCheckErrorHandler, nullCheckHandleError}     = require('./costomModules/customError')
+const createServer                      = require('auto-sni')
 const express                           = require('express')
 const hbs                               = require('express-handlebars')
 const app                               = express()
@@ -41,3 +42,19 @@ app.get('/addressSearch', (request, response) => {
 app.listen(3000, () => {
     console.log('The server is running on Port 3000')
 })
+
+var server = createServer({
+    email: 'hiyong27@gmail.com', // Emailed when certificates expire.
+    agreeTos: true, // Required for letsencrypt.
+    debug: true, // Add console messages and uses staging LetsEncrypt server. (Disable in production)
+    domains: ["shopinshop.tk"], // List of accepted domain names. (You can use nested arrays to register bundles with LE).
+    dir: "~/letsencrypt/etc", // Directory for storing certificates. Defaults to "~/letsencrypt/etc" if not present.
+    ports: {
+        http: 80, // Optionally override the default http port.
+        https: 443 // // Optionally override the default https port.
+    }
+})
+
+server.once("listening", ()=> {
+    console.log("We are ready to go.");
+});
