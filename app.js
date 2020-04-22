eaa41820131f07e4bafcca09019840d7d5eed54c
hiYong94 <chl9741@naver.com>
2020-04-22 17:39:02 +0900

    Fix hbs render error and remove route/index.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const { ErrorHandler, handleError , NullCheckErrorHandler, nullCheckHandleError}     = require('./costomModules/customError')
-const https = require('https')
-const greenlock = require('greenlock-express')
-var createServer = require("auto-sni")
 const express                           = require('express')
 const hbs                               = require('express-handlebars')
 const app                               = express()
 const bodyParser                        = require('body-parser')
-// const router                            = require('./router/index')
 const routerApiV1                       = require('./router/api/v1.0')
 const routerApiV2                       = require('./router/api/v2.0')
+const https                             = require('https') 
+const fs                                = require('fs')
+const options = {
+    key: fs.readFileSync('./keys/private.pem'),
+	cert: fs.readFileSync('./keys/public.pem')
+}
 
 app.engine('hbs', hbs({
     extname: 'hbs',
@@ -22,7 +24,6 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/image', express.static(__dirname + '/public/uploads'))
-app.use(express.static('pulbic'))
 app.use(routerApiV1)
 app.use(routerApiV2)
 
@@ -38,10 +39,13 @@ app.use((error, request, response, next) => {
 })
 
 app.get('/addressSearch', (request, response) => {
-    // response.status(200).render('map/addressSearch.hbs')
-    response.status(200).render('views/map/addressSearch.hbs')
+    response.status(200).render('map/addressSearch.hbs')
 })
 
 app.listen(3000, () => {
     console.log('The server is running on Port 3000')
 })
+
+// https.createServer(options, app).listen(3000, function() {
+//     console.log("HTTPS server listening on port " + 3000);
+// })
