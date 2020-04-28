500a11907938cc4adb1c366db49a3e8a05c832b0
hiYong94 <chl9741@naver.com>
2020-04-28 16:00:42 +0900

    Refactor 'search'
    * Refactor 3 layer
    * Fix service layer request value and validate to controller layer request value and validate
    * Fix service layer response result to controller layer response result

--- controller/v1.0/searchController.js
+++ controller/v1.0/searchController.js
@@ -1,13 +1,67 @@
-const Search = require('../../service/v1.0/searchService')
+const { ErrorHandler }      = require('../../costomModules/customError')
+const isEmpty               = require('../../costomModules/valueCheck')
+const util                  = require('../../costomModules/util')
+const Search                = require('../../service/v1.0/searchService')
 
 exports.searchByPlaceName = function(request, response, next) {
-    Search.searchByPlaceName(request, response, next)
+    let name = request.params.name
+    const nullValueCheckObject = {
+        name
+    }
+    isEmpty(nullValueCheckObject)
+
+    Search.searchByPlaceName([name], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+
+        if(results == 404) {
+            response.status(404).send("No Results Found")
+        } else {
+            util.resultStringToArray(results, ['keywordName', 'imageNumber', 'savedImageName'])
+            response.status(200).send(results)
+        }
+    })
 }
 
 exports.searchByAreaName = function(request, response, next) {
-    Search.searchByAreaName(request, response, next)
+    let name = request.params.name
+    const nullValueCheckObject = {
+        name
+    }
+    isEmpty(nullValueCheckObject)
+
+    Search.searchByAreaName([name], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+        
+        if(results == 404) {
+            response.status(404).send("No Results Found")
+        } else {
+            util.resultStringToArray(results, ['keywordName', 'imageNumber', 'savedImageName'])
+            response.status(200).send(results)
+        }
+    })
 }
 
 exports.searchByKeywordName = function(request, response, next) {
-    Search.searchByKeywordName(request, response, next)
+    let name = request.params.name
+    const nullValueCheckObject = {
+        name
+    }
+    isEmpty(nullValueCheckObject)
+
+    Search.searchByKeywordName([name], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+        
+        if(results.length == 0) {
+            response.status(404).send("No Results Found")
+        } else {
+            util.resultStringToArray(results, ['keywordName', 'imageNumber', 'savedImageName'])
+            response.status(200).send(results)
+        }
+    })
 }
