a646780cb6bd2f8218467fe8ed9ea72ab4e1040b
hiYong94 <chl9741@naver.com>
2020-04-28 17:34:40 +0900

    Refactor 'review'
    * Refactor 3 layer
    * Fix service layer request value and validate to controller layer request value and validate
    * Fix service layer response result to controller layer response result

--- controller/v1.0/reviewController.js
+++ controller/v1.0/reviewController.js
@@ -1,29 +1,156 @@
-const Review = require('../../service/v1.0/reviewService.js')
+const Review                = require('../../service/v1.0/reviewService.js')
+const isEmpty               = require('../../costomModules/valueCheck')
+const { ErrorHandler }      = require('../../costomModules/customError')
+const util                  = require('../../costomModules/util')
 
 exports.reviewCreate = function(request, response, next) {
-    Review.reviewCreate(request, response, next)
+    let placeNumber = request.params.placeNumber
+    let images = request.files
+    const setValues = {
+        memberNumber, content
+    } = request.body
+    
+    const nullValueCheckObject = {
+        images, content
+    }
+    isEmpty(nullValueCheckObject)
+
+    let originalImageName, savedImageName, mimetype, imageSize
+    
+    let iamgesArraySize = images.length
+    let setImagesValues = new Array(iamgesArraySize)
+    for (i = 0; i < iamgesArraySize; i++) {
+        setImagesValues[i] = new Array(4)
+    }
+
+    images.forEach((item, index, array) => {
+        originalImageName = array[index] = item.originalname
+        savedImageName = array[index] = item.filename
+        mimetype = array[index] = item.mimetype
+        imageSize = array[index] = item.size
+        
+        setImagesValues[index][0] = originalImageName
+        setImagesValues[index][1] = savedImageName
+        setImagesValues[index][2] = mimetype
+        setImagesValues[index][3] = imageSize
+    })
+    
+    Review.reviewCreate([placeNumber, memberNumber, content, setImagesValues], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+
+        if(results == 404) {
+            response.status(404).send("Create place duplicate")
+        } else {
+            response.status(200).send(results)
+        }
+    })
 }
 
 exports.reviewReadAll = function(request, response, next) {
-    Review.reviewReadAll(request, response, next)
+    Review.reviewReadAll(function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+        
+        response.status(200).send(results)
+    })
 }
 
 exports.reviewReadByUser = function(request, response, next) {
-    Review.reviewReadByUser(request, response, next)
+    let memberNumber = request.params.memberNumber
+
+    Review.reviewReadByUser([memberNumber], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+
+        util.resultStringToArray(results, ['imageNumber', 'originalImageName', 'savedImageName'])
+        response.status(200).send(results)
+    })
 }
 
 exports.reviewReadOneByPlace = function(request, response, next) {
-    Review.reviewReadOneByPlace(request, response, next)
+    let placeNumber = request.params.placeNumber
+    let reviewNumber = request.params.reviewNumber
+
+    Review.reviewReadOneByPlace([placeNumber, reviewNumber], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+        
+        util.resultStringToArray(results[0], ['imageNumber', 'originalImageName', 'savedImageName'])
+        response.status(200).send(results[0])   
+    })
 }
 
 exports.reviewReadByPlace = function(request, response, next) {
-    Review.reviewReadByPlace(request, response, next)
-}
+    let placeNumber = request.params.placeNumber
 
-exports.reviewUpdate = function(request, response, next) {
-    Review.reviewUpdate(request, response, next)
+    Review.reviewReadByPlace([placeNumber], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+
+        util.resultStringToArray(results, ['imageNumber', 'originalImageName', 'savedImageName'])
+        response.status(200).send(results)
+    })
 }
 
 exports.reviewDelete = function(request, response, next) {
-    Review.reviewDelete(request, response, next)
+    let memberNumber = request.body.memberNumber
+    const setValues = {
+        placeNumber, reviewNumber
+    } = request.params
+    
+    const nullValueCheckObject = {
+        placeNumber, reviewNumber, memberNumber
+    }
+    isEmpty(nullValueCheckObject)
+
+    Review.reviewDelete([memberNumber, placeNumber, reviewNumber], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+
+        response.status(200).send(results)
+    })
+}
+
+exports.reviewUpdate = function(request, response, next) {
+    let images = request.files
+    let placeNumber = request.params.placeNumber
+    let reviewNumber = request.params.reviewNumber
+
+    const setValues = {
+        memberNumber, content, deleteImageNumber,
+    } = request.body
+
+    const nullValueCheckObject = {
+        placeNumber, reviewNumber, memberNumber, content
+    }
+
+    isEmpty(nullValueCheckObject)
+
+    Review.reviewUpdate([images, placeNumber, reviewNumber, memberNumber, content, deleteImageNumber], function(error, results) {
+        if (error) {
+            return next(new ErrorHandler(500, error))
+        }
+        if (results == 404) {
+            response.status(200).send('Review does not exsit')
+        }
+
+        if (results == 200) {
+            response.status(200).send('Update success !!!')
+        }
+
+        if (results == 500) {
+            response.status(500).send('Update fail !!!')
+        }
+
+        if (results == 'NOT MATCH') {
+            response.status(200).send('The writer of the review does not match')
+        }
+    })
 }
--- service/v1.0/reviewService.js
+++ service/v1.0/reviewService.js
@@ -4,224 +4,176 @@ const Review                = require('../../dao/v1.0/reviewDao.js')
 const util                  = require('../../costomModules/util')
 const Step					= require('step')
 
-exports.reviewCreate = function(request, response, next) { 
-    let placeNumber = request.params.placeNumber
-    let images = request.files
-    const setValues = {
-        memberNumber, content
-    } = request.body
-    
-    const nullValueCheckObject = {
-        images, content
-    }
-    isEmpty(nullValueCheckObject)
-
-    let originalImageName, savedImageName, mimetype, imageSize
-    
-    let iamgesArraySize = images.length
-    let setImagesValues = new Array(iamgesArraySize)
-    for (i = 0; i < iamgesArraySize; i++) {
-        setImagesValues[i] = new Array(4)
-    }
-
-    images.forEach((item, index, array) => {
-        originalImageName = array[index] = item.originalname
-        savedImageName = array[index] = item.filename
-        mimetype = array[index] = item.mimetype
-        imageSize = array[index] = item.size
-        
-        setImagesValues[index][0] = originalImageName
-        setImagesValues[index][1] = savedImageName
-        setImagesValues[index][2] = mimetype
-        setImagesValues[index][3] = imageSize
-    })
-
-    let parsePlaceNumber = parseInt(placeNumber)
-    Step (
-        function reviewDuplicateCheck() {
-            Review.selectReviewDuplicateCheck([memberNumber, placeNumber, content], this)
-        },
-        function reviewInsert(error, result) {
-            if (error) {
-				throw new ErrorHandler(500, error)
-            }
-            
-            if(result.length == 0 || result.length == undefined) {
-                Review.insertReview([placeNumber, memberNumber, content, setImagesValues], function(error, results) {
-                    if (error) {
-                        return next(new ErrorHandler(500, error))
-                    }
-                    response.status(200).send(results)
-                })
-            } else {
-                response.status(400).send("Create place duplicate")
-            }
-        }
-    )
-}
-
-exports.reviewReadAll = function(request, response, next) {
-    Review.selectAllReview(function(error, results) {
-        if (error) {
-            return next(new ErrorHandler(500, error))
-        }
-        response.status(200).send(results)
-    })
-}
-
-exports.reviewReadByUser = function(request, response, next) {
-    let memberNumber = request.params.memberNumber
-
-    Review.selectByUser([memberNumber], function(error, results) {
-        if (error) {
-            return next(new ErrorHandler(500, error))
-        }
-        
-        util.resultStringToArray(results, ['imageNumber', 'originalImageName', 'savedImageName'])
-        response.status(200).send(results)
-    })
-}
-
-exports.reviewReadOneByPlace = function(request, response, next) {
-    let placeNumber = request.params.placeNumber
-    let reviewNumber = request.params.reviewNumber
-    
-    Review.selectByReview([placeNumber, reviewNumber], function(error, results) {
-        if (error) {
-            return next(new ErrorHandler(500, error))
-        }
-        
-        util.resultStringToArray(results[0], ['imageNumber', 'originalImageName', 'savedImageName'])
-        response.status(200).send(results[0])        
-    })
-}
-
-exports.reviewReadByPlace = function(request, response, next) {
-    let placeNumber = request.params.placeNumber
-
-    Review.selectByPlace([placeNumber], function(error, results) {
-        if (error) {
-            return next(new ErrorHandler(500, error))
-        }
-        util.resultStringToArray(results, ['imageNumber', 'originalImageName', 'savedImageName'])
-        response.status(200).send(results)
-    })
-}
-
-exports.reviewDelete = function(request, response, next) {
-    let memberNumber = request.body.memberNumber
-    const setValues = {
-        placeNumber, reviewNumber
-    } = request.params
-    
-    const nullValueCheckObject = {
-        placeNumber, reviewNumber, memberNumber
-    }
-    isEmpty(nullValueCheckObject)
-
-    Review.deleteReview([memberNumber, placeNumber, reviewNumber], function(error, results) {
-        if (error) {
-            return next(new ErrorHandler(500, error))
-        }
-        response.status(200).send(results)
-    })
-}
-
-exports.reviewUpdate = function(request, response, next) {
-    let images = request.files
-    let placeNumber = request.params.placeNumber
-    let reviewNumber = request.params.reviewNumber
-
-    const setValues = {
-        memberNumber, content, deleteImageNumber,
-    } = request.body
-
-    const nullValueCheckObject = {
-        placeNumber, reviewNumber, memberNumber, content
-    }
-
-    isEmpty(nullValueCheckObject)
-
-    Step (
-        function reviewCheck() {
-            Review.selectByReview([placeNumber, reviewNumber], this)
-        },
-        function reviewContentUpdate(error, result) {
-            if (error) {
-                throw new ErrorHandler(404, "Review does not exsit")
-            }
-
-            if (result[0].memberNumber == memberNumber) {
-                Review.updateReview([content, reviewNumber], this)
-            } else {
-                response.status(200).send("The writer of the review does not match")
-            }
-        },
-        function reviewImageCheck(error, result) {
-            if (error) {
-                throw new ErrorHandler(500, error)
-            }
-
-            if (result.affectedRows > 0) {
-                Review.selectReviewImages([reviewNumber], this)
-            } else {
-                response.status(500).send("Review Update error")
-            }
-        }, function reviewImageUpdate(error, result) {
-            if (error) {
-                throw new ErrorHandler(404, "Review does not exsit")
-            }
-
-            let queryResultFlag = true
-
-            if (images.length > 0) {
-                let originalImageName, savedImageName, mimetype, imageSize
-                let imagesArraySize = (images != undefined) ? images.length : 0
-                let setImagesValues = new Array(imagesArraySize)
-
-                for(i = 0; i < imagesArraySize; i++) {
-                    setImagesValues[i] = new Array(4)
+exports.reviewCreate = function([placeNumber, memberNumber, content, setImagesValues], response, next) { 
+    try {
+        Step (
+            function reviewDuplicateCheck() {
+                Review.selectReviewDuplicateCheck([memberNumber, placeNumber, content], this)
+            },
+            function reviewInsert(error, result) {
+                if (error) {
+                    throw new ErrorHandler(500, error)
                 }
 
-                for(i = 0; i < imagesArraySize; i++) {
-                    originalImageName = images[i].originalname
-                    savedImageName = images[i].filename
-                    mimetype = images[i].mimetype
-                    imageSize = images[i].size
-                    
-                    setImagesValues[i][0] = originalImageName
-                    setImagesValues[i][1] = savedImageName
-                    setImagesValues[i][2] = mimetype
-                    setImagesValues[i][3] = imageSize
+                if(result.length == 0 || result.length == undefined) {
+                    Review.insertReview([placeNumber, memberNumber, content, setImagesValues], function(error, results) {
+                        if (error) { return response(error, null) }
+                        response(null, results)
+                    })
+                } else {
+                    return response(null, 404)
+                }
+            }
+        )
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
+}
 
-                    setImagesValues[i].unshift(reviewNumber)
+exports.reviewReadAll = function(response, next) {
+    try {
+        Review.selectAllReview(function(error, results) {
+            if (error) { return response(error, null) }
+            response(null, results)
+        })
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
+}
+
+exports.reviewReadByUser = function([memberNumber], response, next) {
+    try {
+        Review.selectByUser([memberNumber], function(error, results) {
+            if (error) { return response(error, null) }
+            response(null, results)
+        })
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
+}
+
+exports.reviewReadOneByPlace = function([placeNumber, reviewNumber], response, next) {
+    try {
+        Review.selectByReview([placeNumber, reviewNumber], function(error, results) {
+            if (error) { return response(error, null) }
+            response(null, results)    
+        })
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
+}
+
+exports.reviewReadByPlace = function([placeNumber], response, next) {
+    try {
+        Review.selectByPlace([placeNumber], function(error, results) {
+            if (error) { return response(error, null) }
+            response(null, results)  
+        })
+    } catch (error) {
+        throw new ErrorHandler(500, error)   
+    }
+}
+
+exports.reviewDelete = function([memberNumber, placeNumber, reviewNumber], response, next) {
+    try {
+        Review.deleteReview([memberNumber, placeNumber, reviewNumber], function(error, results) {
+            if (error) { return response(error, null) }
+            response(null, results) 
+        })
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
+}
+
+exports.reviewUpdate = function([images, placeNumber, reviewNumber, memberNumber, content, deleteImageNumber], response, next) {
+    try {
+        Step (
+            function reviewCheck() {
+                Review.selectByReview([placeNumber, reviewNumber], this)
+            },
+            function reviewContentUpdate(error, result) {
+                if (error) {
+                    throw new ErrorHandler(500, error)
+                }
+
+                if (result.length == 0) {
+                    return response(null, 404)
+                }
+
+                if (result[0].memberNumber != memberNumber) {
+                    return response(null, "NOT MATCH")
+                } else {
+                    Review.updateReview([content, reviewNumber], this)
+                }
+            },
+            function reviewImageCheck(error, result) {
+                if (error) {
+                    throw new ErrorHandler(500, error)
+                }
+
+                if (result.affectedRows > 0) {
+                    Review.selectReviewImages([reviewNumber], this)
+                } else {
+                    return response(null, 500)
+                }
+            }, function reviewImageUpdate(error, result) {
+                if (error) {
+                    return response(null, 404)
+                }
+    
+                let queryResultFlag = true
+    
+                if (images.length > 0) {
+                    let originalImageName, savedImageName, mimetype, imageSize
+                    let imagesArraySize = (images != undefined) ? images.length : 0
+                    let setImagesValues = new Array(imagesArraySize)
+    
+                    for(i = 0; i < imagesArraySize; i++) {
+                        setImagesValues[i] = new Array(4)
+                    }
+    
+                    for(i = 0; i < imagesArraySize; i++) {
+                        originalImageName = images[i].originalname
+                        savedImageName = images[i].filename
+                        mimetype = images[i].mimetype
+                        imageSize = images[i].size
+                        
+                        setImagesValues[i][0] = originalImageName
+                        setImagesValues[i][1] = savedImageName
+                        setImagesValues[i][2] = mimetype
+                        setImagesValues[i][3] = imageSize
+    
+                        setImagesValues[i].unshift(reviewNumber)
+                    }
+                    
+                    Review.insertReviewImages([setImagesValues], function(error, results) {
+                        if (error) { return response(error, null) }
+                        queryResultFlag = (results.affectedRows > 0) ? true : false
+                    })
                 }
                 
-                Review.insertReviewImages([setImagesValues], function(error, results) {
-                    if (error) { return next(new ErrorHandler(500, error)) }
-                    queryResultFlag = (results.affectedRows > 0) ? true : false
-                })
+                if (deleteImageNumber.length > 0) {
+                    Review.deleteReviewImages([reviewNumber, deleteImageNumber], function(error, results) {
+                        if (error) { return response(error, null) }
+                        queryResultFlag = (results.affectedRows > 0) ? true : false
+                    })
+                }
+    
+                return queryResultFlag
+            },
+            function queryResults(error, result) {
+                if (error) {
+                    throw new ErrorHandler(500, error)
+                }
+    
+                if (result == true) {
+                    return response(null, 200)
+                } else {
+                    return response(null, 500)
+                }
             }
-            
-            if (deleteImageNumber.length > 0) {
-                Review.deleteReviewImages([reviewNumber, deleteImageNumber], function(error, results) {
-                    if (error) { return next(new ErrorHandler(500, error)) }
-                    queryResultFlag = (results.affectedRows > 0) ? true : false
-                })
-            }
-
-            return queryResultFlag
-        },
-        function queryResults(error, result) {
-            if (error) {
-                throw new ErrorHandler(500, error)
-            }
-
-            if (result == true) {
-                response.status(200).send('Update success !!!')
-            } else {
-                response.status(500).send('Update fail !!!')
-            }
-        }
-    )
+        )
+    } catch (error) {
+        throw new ErrorHandler(500, error)
+    }
 }
